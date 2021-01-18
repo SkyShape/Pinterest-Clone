472cfc23651d86276326af69af657b7337a6b63f
Haba Vlad <vlad02cristi@gmail.com>
2021-01-18 04:55:15 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 
 import './App.css';
 import Header from "./Header";
+import Mainboard from "./Mainboard";
 
 function App() {
   return (
     <div className="app">
-      <Header />
+        <div className="app_header">
+          <Header />
+        </div>
+
+        <div className="app_mainboard">
+          <Mainboard />
+        </div>
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import "./Header.css";
+import PinterestIcon from '@material-ui/icons/Pinterest';
+import SearchIcon from '@material-ui/icons/Search';
 
 function Header() {
     return (
         <div className="app_header">
             <div className="header_wrapper">
                     <div className="header_logo">
-                        <p>Logo</p>
+                        <PinterestIcon />
                     </div>
 
                     <div className="header_button homePage">
@@ -19,7 +21,7 @@ function Header() {
 
                     <div className="header_search">
                         <div className="header_searchContainer">
-                            <p>SearchIcon</p>
+                            <SearchIcon />
                             <input type="text"/>
                             <button>Submit</button>
                         </div>
